f9eca8d60986118fc059c6849f3ff62556146254
vennela1002 <114627388+vennela1002@users.noreply.github.com>
2022-11-21 17:30:29 -0500

    changes in css styling

--- Assignment 9/assignment9/src/App.js
+++ Assignment 9/assignment9/src/App.js
@@ -1,46 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./pages/Navbar";
+import Login from "./pages/Login";
+import Home from  "./pages/Home";
+import Jobs from  "./pages/Jobs"; 
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+  <div className= "App">
+  <Router>
+  {/* <Login/> */}
+  <Routes>
+    
+  <Route path = "/" element = {<Login/>}></Route>
+  <Route path = "/Home" element = {<Home/>}></Route>
+  <Route path = "/Jobs" element = {<Jobs/>}></Route>
+  <Route path = "/about" element = {<About/>}></Route>
+  <Route path = "/contact" element = {<Contact/>}></Route>
+  </Routes>
+  </Router>
+  </div>
+
   );
-}
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-export default App;
+  }
+  export default App;
